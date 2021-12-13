dc3b655bfd9171fe3ac8e23b862c328ac2028555
Anderson Pimentel <andersonluiscp@gmail.com>
2021-12-12 22:05:31 -0300

    change the mocked tiles map

--- app/src/main/java/br/andersonpimentel/littleelephant/domain/usecases/GetMockedMap.kt
+++ app/src/main/java/br/andersonpimentel/littleelephant/domain/usecases/GetMockedMap.kt
@@ -3,22 +3,21 @@ package br.andersonpimentel.littleelephant.domain.usecases
 import br.andersonpimentel.littleelephant.domain.entities.Tile
 
 interface GetMockedMapInterface {
-    operator fun invoke(): List<Tile>
+    fun execute(): List<Tile>
 }
 
 class GetMockedMap: GetMockedMapInterface {
-    override fun invoke(): List<Tile> {
-        val tiles = listOf<Tile>(
-            Tile.TreeTile, Tile.GrassTile, Tile.RoadVerticalTile, Tile.GrassTile,
-            Tile.GrassTile, Tile.GrassTile, Tile.StepTile(Tile.Orientation.VERTICAL, 1), Tile.GrassTile,
-            Tile.GrassTile, Tile.TopTurnLeftTile, Tile.BottomTurnLeftTile, Tile.GrassTile,
-            Tile.GrassTile, Tile.StepTile(Tile.Orientation.VERTICAL, 2), Tile.GrassTile, Tile.TreeTile,
-            Tile.GrassTile, Tile.BottomTurnRightTile, Tile.RoadHorizontalTile, Tile.TopTurnRightTile,
-            Tile.GrassTile, Tile.GrassTile, Tile.TreeTile, Tile.RoadVerticalTile,
-            Tile.GrassTile, Tile.TopTurnLeftTile, Tile.StepTile(Tile.Orientation.HORIZONTAL, 3), Tile.BottomTurnLeftTile,
-            Tile.TreeTile, Tile.RoadVerticalTile, Tile.GrassTile, Tile.GrassTile,
-            Tile.GrassTile, Tile.StepTile(Tile.Orientation.VERTICAL, 4), Tile.GrassTile, Tile.GrassTile
+    override fun execute(): List<Tile> {
+        return listOf(
+            Tile.GrassTile(hasTree = true), Tile.GrassTile(), Tile.RoadVerticalTile, Tile.GrassTile(),
+            Tile.GrassTile(), Tile.GrassTile(), Tile.StepTile(Tile.Orientation.VERTICAL, 1), Tile.GrassTile(),
+            Tile.GrassTile(), Tile.TopToLeftTile, Tile.BottomToLeftTile, Tile.GrassTile(),
+            Tile.GrassTile(), Tile.StepTile(Tile.Orientation.VERTICAL, 2), Tile.GrassTile(), Tile.GrassTile(hasTree = true),
+            Tile.GrassTile(), Tile.BottomToRightTile, Tile.RoadHorizontalTile, Tile.TopToRightTile,
+            Tile.GrassTile(), Tile.GrassTile(), Tile.GrassTile(hasTree = true), Tile.RoadVerticalTile,
+            Tile.GrassTile(), Tile.TopToLeftTile, Tile.StepTile(Tile.Orientation.HORIZONTAL, 3), Tile.BottomToLeftTile,
+            Tile.GrassTile(hasTree = true), Tile.RoadVerticalTile, Tile.GrassTile(), Tile.GrassTile(),
+            Tile.GrassTile(), Tile.StepTile(Tile.Orientation.VERTICAL, 4), Tile.GrassTile(), Tile.GrassTile()
         )
-        return tiles
     }
 }
