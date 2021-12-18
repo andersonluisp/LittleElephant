c7edca358a4b5a758a7c8baad85287c6b17732fd
Anderson Pimentel <andersonluiscp@gmail.com>
2021-12-18 16:37:03 -0300

    Update the GetMapUseCase to zip the lastElephantPosition into the Map

--- app/src/main/java/br/andersonpimentel/littleelephant/domain/usecases/GetMapUseCase.kt
+++ app/src/main/java/br/andersonpimentel/littleelephant/domain/usecases/GetMapUseCase.kt
@@ -2,11 +2,15 @@ package br.andersonpimentel.littleelephant.domain.usecases
 
 import br.andersonpimentel.littleelephant.domain.entities.Map
 import br.andersonpimentel.littleelephant.domain.entities.Tile
-import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.zip
 
 class GetMapUseCase(
     private val useCaseGetStepMessages: GetStepMessagesUseCase,
-    private val useCaseGetTiles: GetTilesUseCase
+    private val useCaseGetTiles: GetTilesUseCase,
+    private val useCaseGetLastElephantPosition: GetLastElephantPositionUseCase
     ) {
     suspend operator fun invoke(): Flow<Map> {
         return flowOf(Map(
@@ -18,6 +22,16 @@ class GetMapUseCase(
                                 tile.message = messages.list[index].message
                             } else {
                                 //TODO Define
+                            }
+                        }
+                    }
+                }
+            }.zip(useCaseGetLastElephantPosition()){ tiles, lastElephantPosition ->
+                tiles.apply {
+                    if (lastElephantPosition is GetLastElephantPositionUseCase.ResultElephantPosition.ElephantPosition){
+                        filterIsInstance<Tile.StepTile>().forEach { tile ->
+                            if (tile.step == lastElephantPosition.lastElephantPosition){
+                                tile.hasElephant = true
                             }
                         }
                     }
